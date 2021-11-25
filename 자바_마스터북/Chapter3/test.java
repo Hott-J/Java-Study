8eb349985cd5e789f853e1b91293f5449edd4341
Hott-J <jhj13062004@naver.com>
2021-11-25 19:47:10 +0900

    init

--- 자바_마스터북/Chapter3/test.java
+++ 자바_마스터북/Chapter3/test.java
@@ -1,8 +1,94 @@
-package 자바_마스터북.Chapter3;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class test{
-    public static void main(String[] args) {
-        Integer num01 = new Integer(10); // 새로운 객체를 생성하기에 비효율적
-        Integer num02 = Integer.valueOf(10); // 캐시된 객체를 반환
-    }
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+
+		Node[] node = new Node[N];
+		for (int j = 0; j < N; j++) {
+			node[j] = new Node((char)(j+65));
+		}
+//		Arrays.setAll(node, i -> new Node((char)(i+65));  for문 초기화보다 속도가 많이 느림.
+		Tree tree = new Tree(node[0]);
+
+		while(N-- > 0){
+			st = new StringTokenizer(br.readLine());
+			char n1 = st.nextToken().charAt(0);
+			char n2 = st.nextToken().charAt(0);
+			char n3 = st.nextToken().charAt(0);
+
+			if(n2 != '.'){
+				node[n1-65].left = node[n2-65];
+				node[n2-65].parent = node[n1-65];
+			}
+
+			if(n3 != '.'){
+				node[n1-65].right = node[n3-65];
+				node[n3-65].parent = node[n1-65];
+			}
+		}
+
+		tree.preOrder(tree.root, sb);
+		sb.append('\n');
+		tree.inOrder(tree.root, sb);
+		sb.append('\n');
+		tree.postOrder(tree.root, sb);
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+}
+class Node{
+	char element;
+	Node parent, left, right;
+	public Node(char e){
+		this.element = e;
+		parent = null;
+		left = null;
+		right = null;
+	}
+}
+class Tree{
+	Node root;
+	Tree(Node v){
+		root = v;
+	}
+
+	public void preOrder(Node v, StringBuilder sb){
+		sb.append(v.element);
+		if(v.left != null)
+			preOrder(v.left, sb);
+
+		if(v.right != null)
+			preOrder(v.right, sb);
+	}
+
+	public void inOrder(Node v, StringBuilder sb){
+		if(v.left != null)
+			inOrder(v.left, sb);
+
+		sb.append(v.element);
+
+		if(v.right != null)
+			inOrder(v.right, sb);
+	}
+
+	public void postOrder(Node v, StringBuilder sb){
+		if(v.left != null)
+			postOrder(v.left, sb);
+
+		if(v.right != null)
+			postOrder(v.right, sb);
+		sb.append(v.element);
+	}
 }
--- 자바_마스터북/Chapter5/test.java
+++ 자바_마스터북/Chapter5/test.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public class test{
     public static void main(String[] args) {
-        Integer[] integerArray = IntStream.of(62,8,31).boxed()
-        .toArray(n -> new Integer[n]); 
-        //.collect(Collectors.toList());
-        System.out.println(integerArray.length);
+        // Integer[] integerArray = IntStream.of(62,8,31).boxed()
+        // .toArray(n -> new Integer[n]); 
+        // //.collect(Collectors.toList());
+        // System.out.println(integerArray.length);
 
-        List<Integer> integerList = IntStream.of(1,62,31).boxed()
-        .collect(Collectors.toList());
-        System.out.println(integerList);
+        // List<Integer> integerList = IntStream.of(1,62,31).boxed()
+        // .collect(Collectors.toList());
+        // System.out.println(integerList);
+        // System.out.print(Long.SIZE);
+        String a ="123";
+        char b = 'a';
+        String c = "123";
+        String d = "543";
+        c = d;
+
+        char e = '\u0000';
+        System.out.print(e);
     }
 }
